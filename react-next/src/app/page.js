cd04ba52695ea3135d54957d343fd30d4420ba7a
FelipeKauanG <felipekauanprofissional@gmail.com>
2023-09-21 09:08:50 -0300

    v1.1

--- react-next/src/app/page.js
+++ react-next/src/app/page.js
@@ -1,13 +1,11 @@
 
 import MyApp from './components/MyApp';
-import MyButton from './components/MyButton';
 
 export default function Home() {
   return (
     <main>
   
       <MyApp />
-      <MyButton />
     </main>
   )
 }
